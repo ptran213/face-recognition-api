5fffd5fb2158b30b3fb683981cef61b6135ae2e9
Ivan Tran <phuongtran1312@gmail.com>
2018-12-03 08:21:31 -0800

    Fixing Bug with nodemon

--- server.js
+++ server.js
@@ -31,6 +31,8 @@ app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
-app.listen(process.env.PORT || 3000, ()=> {
+const DATABASE_URL = process.env.DATABASE_URL
+
+app.listen(3000, ()=> {
   console.log('app is running on port 3000');
 })
